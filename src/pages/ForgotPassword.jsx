c612cc8607dda5e43ac6b6f24a87643b393dc093
Saúl Favela <saulfavela01@gmail.com>
2024-02-16 13:04:52 -0600

    Se completó el archivo ForgotPassword.jsx, éste realiza validaciones en la contraseña para comprobar que sean iguales, además de comprobar que la nueva contraseña sea mayor a 8 caracteres e incluya al menos una mayúscula, una minúscula, un número y un caracter especial

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -7,53 +7,62 @@ const ForgotPassword = () => {
   const [step, setStep] = useState(1);
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState("");
-  const [addressLine1, setAddressLine1] = useState("");
-  const [addressLine2, setAddressLine2] = useState("");
+  const [otpCode, setOtpCode] = useState("");
+  const [isOtpButtonDisabled, setIsOtpButtonDisabled] = useState(false);
+  const [countdown, setCountdown] = useState(60);
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const [isValidNewPassword, setIsValidNewPassword] = useState(true);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
     setEmailError("");
   };
-
-  const handleAddressLine1Change = (event) => {
-    setAddressLine1(event.target.value);
+  const handleNewPasswordChange = (event) => {
+    const newPasswordValue = event.target.value;
+    const isValidPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(newPasswordValue);
+    setIsValidNewPassword(isValidPassword);
+    setNewPassword(newPasswordValue);
   };
-
-  const handleAddressLine2Change = (event) => {
-    setAddressLine2(event.target.value);
+  const handleConfirmPasswordChange = (event) => {
+    const confirmPasswordValue = event.target.value;
+    setPasswordsMatch(confirmPasswordValue === newPassword);
+    setConfirmPassword(confirmPasswordValue);
   };
-
+  const handleOtpCodeChange = (event) => {
+    setOtpCode(event.target.value);
+  };
   const handleSubmitStep1 = (event) => {
     event.preventDefault();
     if (!isValidEmail(email)) {
       setEmailError("Invalid email address");
     } else {
       setEmailError("");
-      setStep(step + 1);
+      setStep(2);
     }
   };
-
   const handleSubmitStep2 = (event) => {
     event.preventDefault();
-    // Handle submission for step 2
+    // Realizar validaciones necesarias para el paso 2
+    // Si las validaciones son exitosas, establecer el paso en 3
+    setStep(3);
   };
-
-  const nextStep = () => {
-    if (step === 1) {
-      handleSubmitStep1();
-    } else {
-      setStep(step + 1);
-    }
+  const handleSubmitStep3 = (event) => {
+    event.preventDefault();
+    if (newPassword !== confirmPassword) {
+      setPasswordsMatch(false);
+      return;
+    };
+    setPasswordsMatch(true);
+    // Continuar con el envío del formulario si las contraseñas coinciden
   };
-
   const prevStep = () => {
     setStep(step - 1);
   };
-
   const isValidEmail = (email) => {
     return /\S+@\S+\.\S+/.test(email);
   };
-
   useEffect(() => {
     const imagen = document.getElementById("logo");
     if (imagen) {
@@ -64,6 +73,25 @@ const ForgotPassword = () => {
       imagen.style.marginBottom = "20px";
     }
   }, []);
+  const handleSendOtp = () => {
+    // Simulate sending OTP message
+    console.log("OTP sent!");
+    setIsOtpButtonDisabled(true); // Disable the button
+    setCountdown(60); // Reset countdown
+    const countdownInterval = setInterval(() => {
+      setCountdown((prevCountdown) => {
+        if (prevCountdown === 1) {
+          clearInterval(countdownInterval); // Stop countdown when it reaches 0
+          setIsOtpButtonDisabled(false); // Enable the button
+        }
+        return prevCountdown - 1; // Decrement countdown
+      });
+    }, 1000); // Update countdown every second
+  };
+  useEffect(() => {
+    // Clean up interval on component unmount
+    return () => clearInterval();
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -71,10 +99,10 @@ const ForgotPassword = () => {
         <a href="/HomePage">
           <img id="logo" src="/src/images/logoCLA.png" alt="Community Lab Alliance Logo" />
         </a>
-        <h1 className="text-xl font-bold mb-4">Forgot Password</h1>
 
         {step === 1 && (
           <form onSubmit={handleSubmitStep1}>
+            <h1 className="text-xl font-bold mb-4">Forgot Password</h1>
             <div className="mb-4">
               <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-1">
                 Email <span className="text-red-500">*</span>
@@ -89,74 +117,117 @@ const ForgotPassword = () => {
               />
               {emailError && <p className="text-red-500 text-sm mt-1">{emailError}</p>}
             </div>
-
             <button
               type="submit"
               className="w-full bg-[#4CB5AB] text-white py-2 rounded-lg hover:bg-[#389389]"
             >
-              Next
+              Send OTP
               <FontAwesomeIcon icon={faArrowRight} className="ml-1 align-middle" />
             </button>
           </form>
         )}
 
         {step === 2 && (
-          <div>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-gray-700 text-sm font-bold mb-1"
-              >
-                Entered email
-              </label>
-              <div className="border border-gray-300 p-2 rounded-lg">
-                {email}
-                <button
-                  className="ml-2 px-3 py-1 bg-gray-200 text-gray-700 rounded-lg"
-                  onClick={prevStep}
-                >
-                  Change
-                </button>
-              </div>
-            </div>
-            <form onSubmit={handleSubmitStep2}>
+          <form onSubmit={handleSubmitStep2}>
+            <h1 className="text-xl font-bold mb-4">Forgot Password</h1>
+            <div>
               <div className="mb-4">
-                <label
-                  htmlFor="addressLine1"
-                  className="block text-gray-700 text-sm font-bold mb-1"
-                >
-                  Address Line 1 <span className="text-red-500">*</span>
+                <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-1">
+                  Entered email
+                </label>
+                <div className="border border-gray-300 p-2 rounded-lg">
+                  {email}
+                  <button
+                    className="ml-2 px-3 py-1 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-400"
+                    onClick={prevStep}
+                  >
+                    Change
+                  </button>
+                </div>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="otpCode" className="block text-gray-700 text-sm font-semibold mb-1">
+                  Enter the one-time password sent to your email address. <span className="text-red-500">*</span>
                 </label>
                 <input
                   type="text"
-                  id="addressLine1"
-                  value={addressLine1}
-                  onChange={handleAddressLine1Change}
-                  placeholder="Enter CAPTCHA"
+                  id="otpCode"
+                  value={otpCode}
+                  onChange={handleOtpCodeChange}
+                  placeholder="• • • • • •"
                   required
-                  className=" border border-gray-300 p-2 rounded-lg"
+                  className="border border-gray-300 p-2 rounded-lg w-1/2"
                 />
-              </div>
-
-              <div className="flex items-center justify-center">
                 <button
-                  type="button"
-                  onClick={prevStep}
-                  className="bg-gray-300 text-gray-700 py-2 px-4 rounded-lg hover:bg-gray-400 mr-2"
+                  onClick={handleSendOtp}
+                  disabled={isOtpButtonDisabled}
+                  className={`bg-[#4CB5AB] hover:bg-[#389389] text-white ml-4 py-2 px-3 rounded-lg ${isOtpButtonDisabled ? "cursor-not-allowed opacity-50" : ""}`}
                 >
-                  Back
+                  {isOtpButtonDisabled ? `Resend OTP (${countdown}s)` : "Resend OTP"}
                 </button>
+              </div>
+              <div className="flex items-center justify-center">
                 <button
                   type="submit"
                   className="bg-[#4CB5AB] text-white py-2 rounded-lg w-full hover:bg-[#389389]"
                 >
-                  Sign Up
+                  Verify
                 </button>
               </div>
-            </form>
-          </div>
+
+            </div>
+          </form>
         )}
 
+        {step === 3 && (
+          <form onSubmit={handleSubmitStep3}>
+            <h1 className="text-xl font-bold mb-4">Create New Password</h1>
+            <div>
+              <div className="mb-4">
+                <label htmlFor="newPassword" className="block text-gray-700 text-sm font-semibold mb-1">
+                  New Password <span className="text-red-500">*</span>
+                </label>
+                <input
+                    type="password"
+                    id="newPassword"
+                    value={newPassword}
+                    onChange={handleNewPasswordChange}
+                    required
+                    className={`w-full border ${!isValidNewPassword ? "border-red-500" : "border-gray-300"} p-2 rounded-lg`}
+                  />
+                  {!isValidNewPassword && (
+                    <p className="text-red-500 text-sm mt-1">Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.</p>
+                  )}
+                </div>
+              <div className="mb-4">
+                <label htmlFor="confirmPassword" className="block text-gray-700 text-sm font-semibold mb-1">
+                  Confirm New Password <span className="text-red-500">*</span>
+                </label>
+                <input
+                    type="password"
+                    id="confirmPassword"
+                    value={confirmPassword}
+                    onChange={handleConfirmPasswordChange}
+                    required
+                    className={`w-full border ${!passwordsMatch ? "border-red-500" : "border-gray-300"} p-2 rounded-lg`}
+                  />
+                  {!passwordsMatch && (
+                    <p className="text-red-500 text-sm mt-1">Passwords do not match.</p>
+                  )}
+                </div>
+              <div className="flex items-center justify-center">
+                <button
+                  type="submit"
+                  className="bg-[#4CB5AB] text-white py-2 rounded-lg w-full hover:bg-[#389389]"
+                >
+                  Change Password
+                </button>
+                <div className="flex items-center justify-center mt-4">
+                </div>
+              </div>
+            </div>
+          </form>
+        )}
         <div className="mt-4 flex items-center justify-center">
           <Link to="/SolutionsPage" className="ml-1 text-[#4CB5AB] hover:text-[#389389] hover:underline">
             Back to Sign In
