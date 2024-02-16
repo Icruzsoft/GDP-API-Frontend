7c93059e91128c7fb8adc5987bcc9b051427f0af
EduardoValenciaG <eduardovg588@gmail.com>
2024-02-15 18:48:17 -0600

    Avanzo 2 de home se lleva 85%

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -22,6 +22,10 @@ import Image14 from "../images/imghome/Logo11.jpg";
 import Image15 from "../images/imghome/Logo12.jpg";
 import Image16 from "../images/imghome/Logo13.jpg";
 import Image17 from "../images/imghome/Logo14.jpg";
+import Image18 from "../images/imghome/ArtiR1.jpg";
+import Image19 from "../images/imghome/ArtiR2.jpg";
+import Image20 from "../images/imghome/ArtiR3.jpg";
+
 
 const NavBar = () => {
   const sliderSettings = {
@@ -44,6 +48,16 @@ const NavBar = () => {
     autoplaySpeed: 3000 // Cambia la imagen cada 2 segundos
   };
 
+  const sliderSettingsThreePerSlide = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4, // Mostrar dos imágenes a la vez
+    slidesToScroll: 4, // Desplazar dos imágenes a la vez
+    autoplay: true,
+    autoplaySpeed: 3000 // Cambia la imagen cada 2 segundos
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-white shadow">
       {/* Contenedor superior con barra de navegación y icono de usuario */}
@@ -59,7 +73,7 @@ const NavBar = () => {
           <nav className="flex space-x-4">
             {/* botones de navegación */}
             <NavLink
-              to="/Dashboard" // Enlace a la página "Dashboard"
+              to="/DashboardPage" // Enlace a la página "Dashboard"
               className="text-gray-700 hover:text-blue-500 transition duration-300"
             >
               Dashboard
@@ -125,7 +139,7 @@ const NavBar = () => {
 
       {/* Cambiar el color y tamaño de las letras */}
       <center>
-        <h1 style={{ color: "blue", fontSize: "35px" }}>Algunos de nuestros clientes</h1>   
+        <h1 style={{ color: "#66BDE0", fontSize: "35px" }}>Algunos de nuestros clientes</h1>   
       </center>
       <br/><br/><br/>
 
@@ -154,7 +168,7 @@ const NavBar = () => {
       </div><br/><br/><br/><br/>
 
       <center>
-        <h1 style={{ color: "green", fontSize: "35px" }}>¿Cómo funciona?</h1>  
+        <h1 style={{ color: "#B3D433", fontSize: "30px" }}>¿Cómo funciona?</h1> <br/> 
 
         <p style={{ color: "black", fontSize: "22px" }}>Somos la única plataforma en español que conecta,gestiona y capacita equipos freelance para ofrecer
           <br/> soluciones integrales para empresas
@@ -166,9 +180,94 @@ const NavBar = () => {
         <img src={Image17} alt="Logo 14" />
       </div>  <br/><br/><br/>
 
-      {/* Rectángulo gris */}
-      <div style={{ backgroundColor: "rgba(242, 242, 242, 0.9)", width: "100%", height: "540px", margin: "auto" }}></div>
+   {/* Rectángulo gris */}
+<div style={{ backgroundColor: "rgba(242, 242, 242, 0.9)", width: "100%", height: "540px", margin: "auto", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}> 
+
+{/* Texto de testimonios */}
+<h1 style={{ color: "#013A51", fontSize: "35px", marginBottom: "20px" }}>Testimonios</h1>
+
+{/* Rectángulo personalizado */}
+<div style={{ backgroundColor: "rgba(242, 242, 242, 0.9)", width: "56%", height: "400px", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", flexWrap: "wrap" }}>
+  {/* Columna 1 */}
+  <div style={{ width: "50%", marginBottom: "20px", textAlign: "center" }}>
+  <h2 style={{ color: "#1F76B7", fontSize: "18px", marginBottom: "20px", fontStyle: "italic" }}>“Aprendí que no quiero ser programadora toda la vida pero que saber sobre Tecnología me va a ayudar en lo que sea que haga”</h2>
+
+    <p style={{ color: "#B3D433", fontSize: "18px", marginBottom: "20px" }} >(Belém, estudiante de preparatoria)</p>
+
+    <p style={{ color: "#1F76B7", fontSize: "18px", marginBottom: "20px", fontStyle: "italic" }} >“En este equipo entendí que un científico también puede trabajar fuera del laboratorio y que los ingenieros necesitan aprender más sobre negocios y mercadotecnia”.</p>
+    
+    <p style={{ color: "#B3D433", fontSize: "18px", marginBottom: "20px" }}> (Francisco- Freelance)</p>
+  </div>
+  
+  {/* Columna 2 */}
+  <div style={{ width: "50%", marginBottom: "20px", textAlign: "center" }}>
+    <h2 style={{ color: "#1F76B7", fontSize: "18px", marginBottom: "20px", fontStyle: "italic" }} >“Gracias a su programa de digitalización tenemos mejor control de nuestros 
+      procesos y documentos, eso nos va a ayudar a crecer más”.</h2>
+
+    <p style={{ color: "#B3D433", fontSize: "18px", marginBottom: "20px" }} >(Ivett, empleada en agencia de viajes)</p>
+
+    <p style={{ color: "#1F76B7", fontSize: "18px", marginBottom: "20px", fontStyle: "italic" }} >“Los productos que adquirimos en el marketplace de Community Lab Alliance son mejores cada día”.</p>
+    
+    <p style={{ color: "#B3D433", fontSize: "18px", marginBottom: "20px" }} >(Fabian- Gerente en empresa de electrocomponentes)</p>
+  </div>
+</div>
+</div>
+
       <br/><br/><br/>
+      <center>
+        <h1 style={{ color: "#B3D433", fontSize: "35px" }}>Aliados</h1>   
+      </center>
+
+      {/* Tercer carrusel */}
+      <div className="flex justify-center mt-8">
+        <Slider {...sliderSettingsThreePerSlide} className="w-full max-w-screen-lg">
+            <img src={Image4} alt="Logo 1" className="mx-2" />
+            <img src={Image5} alt="Logo 2" className="mx-2" />
+            <img src={Image6} alt="Logo 3" className="mx-2" />
+            <img src={Image7} alt="Logo 4" className="mx-2" />
+            <img src={Image8} alt="Logo 5" className="mx-2" />
+            <img src={Image9} alt="Logo 6" className="mx-2" />
+            <img src={Image10} alt="Logo 7" className="mx-2" />
+            <img src={Image11} alt="Logo 8" className="mx-2" />
+            <img src={Image12} alt="Logo 9" className="mx-2" />
+            <img src={Image13} alt="Logo 10" className="mx-2" />
+            <img src={Image14} alt="Logo 11" className="mx-2" />
+            <img src={Image15} alt="Logo 12" className="mx-2" />
+      </Slider>
+      </div>
+      <br/><br/><br/>
+
+      <center>
+        <h1 style={{ color: "#003169", fontSize: "35px" }}>Artículos Recientes</h1>   
+      </center><br/>
+
+   {/* Rectángulo personalizado 2 */}
+   <center>
+  <div style={{ backgroundColor: "rgba(242, 242, 242, 0.9)", width: "75%", minHeight: "500px", display: "flex", justifyContent: "center", alignItems: "flex-start", flexWrap: "wrap" }}>
+    {/* Primera imagen con texto */}
+    <div style={{ flex: "1", display: "flex", alignItems: "center", marginBottom: "20px" }}>
+      <img src={Image18} alt="ArtiR1" style={{ maxWidth: "200px", height: "auto", marginRight: "20px" }} />
+      <p style={{ color: "#333", fontSize: "16px" }}>La Importancia de la Automatización en las Empresas</p>
+    </div>
+    {/* Segunda imagen con texto */}
+    <div style={{ flex: "1", display: "flex", alignItems: "center", marginBottom: "20px" }}>
+      <img src={Image19} alt="rtiR2" style={{ maxWidth: "200px", height: "auto", marginRight: "20px" }} />
+      <p style={{ color: "#333", fontSize: "16px" }}>¿Qué es Asana y para qué sirve?</p>
+    </div>
+    {/* Tercera imagen con texto */}
+    <div style={{ flex: "1", display: "flex", alignItems: "center", marginBottom: "20px" }}>
+      <img src={Image20} alt="ArtiR3" style={{ maxWidth: "200px", height: "auto", marginRight: "20px" }} />
+      <p style={{ color: "#333", fontSize: "16px" }}>La Importancia del Diseño de Museografía Científica</p>
+    </div>
+  </div>
+</center>
+
+
+
+
+
+      
+      <br/><br/><br/><br/><br/>
 
       
     </div>
