7b796ffda86a329a98cba421775c99c59c44897d
ASUS-ISAAC\isaac <isaacsaucedos15@gmail.com>
2024-02-17 19:33:23 -0600

    Modifique el Footer e implemente el Sidebar con el Header del Dashboard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "../images/imghome/Logo15.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faSearch, faBell, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faBell, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
 import Sidebar from '../components/Sidebar';
 
 const BarraVenta = () => {
@@ -9,30 +9,33 @@ const BarraVenta = () => {
   return (
     <div style={{ backgroundColor: "white", borderBottom: "1px solid black", textAlign: "justify", width: "100%", height: "80px", display: "flex", alignItems: "center", justifyContent: "space-between", paddingLeft: "10px", paddingRight: "10px" }}>
       {/* Sección izquierda de la barra, incluye el logo y el botón de menú */}
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <img src={Image} alt="Logo" style={{ marginRight: "10px" }} />
-        <button style={{ border: "none", background: "none", marginRight: "10px" }}>
-          <FontAwesomeIcon icon={faBars} />
-        </button>
-        <div style={{ fontSize: "20px" }}>Community Lab Alliance</div>
+      <div style={{ display: "flex", alignItems: "center"}}>
+        <img src={Image} alt="Logo" style={{ marginRight: "10px", background: "White"}} />
+          <Sidebar/>
+        <div style={{ fontSize: "20px", marginLeft: "35px"}}>Community Lab Alliance</div>
       </div>
       {/* Sección derecha de la barra, incluye el campo de búsqueda y los botones de notificaciones y mensajes */}
       <div style={{ display: "flex", alignItems: "center" }}>
         {/* Campo de búsqueda */}
-        <div style={{ border: "1px solid black", borderRadius: "5px", marginRight: "400px", marginLeft: "auto" }}>
+        <div style={{ border: "1px solid black", borderRadius: "5px", marginRight: "150px", marginLeft: "auto" }}>
           <input type="text" placeholder="Search here" style={{ border: "none", background: "none", outline: "none", padding: "5px" }} />
           <button style={{ border: "none", background: "none" }}>
             <FontAwesomeIcon icon={faSearch} />
           </button>
         </div>
         {/* Botón de notificaciones */}
-        <button style={{ border: "none", background: "none", marginRight: "1px", marginLeft: "1px" }}>
+        <button style={{ border: "none", background: "none", marginRight: "5px", marginLeft: "1px" }}>
           <FontAwesomeIcon icon={faBell} />
         </button>
         {/* Botón de mensajes */}
         <button style={{ border: "none", background: "none", marginRight: "50px", marginLeft: "1px" }}>
           <FontAwesomeIcon icon={faEnvelope} />
         </button>
+        {/* Botón de Perfil */}
+        <button style={{ border: "none", background: "none", marginRight: "15px", marginLeft: "1px" }}>
+          <FontAwesomeIcon icon={faUser} />
+        </button>
+
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,36 +1,61 @@
-import React from 'react';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faGaugeHigh, faCube, faUser, faRocket, faLayerGroup, faMoneyBill1, faClock } from '@fortawesome/free-solid-svg-icons';
 
 function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-        <div className="text-gray-100 text-xl">
+    <div className="text-gray-100 text-xl bg-white">
+      <button
+        className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-slate-400 text-white bg-gray-100"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen}
+        <FontAwesomeIcon icon={faBars} className="ml-2 text-gray-950" />
+      </button>
+
+      {isOpen && (
+        <div className='fixed top-20 left-0 h-screen bg-slate-700 w-60'>          
           <div className='p-2.5 mt-1 flex items-center'>
-            <h1 className='font-bold text-gray-200 text-[15px] ml-3'>Main</h1>
+           <a href="http://localhost:5173" className='font-bold text-gray-200 text-[15px] ml-3'>Main</a>
           </div>
           <hr className='my-2 text-gray-600'></hr>
+          <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-800 text-white'>
+            <FontAwesomeIcon icon={faGaugeHigh} />
+            <span className='text-[15px] ml-4 text-gray-200'>Dashboard</span>
+          </div>
+          <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-800 text-white'>
+            <FontAwesomeIcon icon={faCube} />
+            <span className='text-[15px] ml-4 text-gray-200'>Apps</span>
+          </div>
+          <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-800 text-white'>
+            <FontAwesomeIcon icon={faUser} />
+            <span className='text-[15px] ml-4 text-gray-200'>Expertos</span>
+          </div>
+          <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-800 text-white ml-5'>
+            <span className='text-[15px] ml-4 text-gray-200'>Clientes</span>
+          </div>
+          <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-800 text-white'>
+            <FontAwesomeIcon icon={faRocket} />
+            <span className='text-[15px] ml-4 text-gray-200'>Proyectos</span>
+          </div>
+          <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-800 text-white'>
+            <FontAwesomeIcon icon={faLayerGroup} />
+            <span className='text-[15px] ml-4 text-gray-200'>Pre Diagnostico</span>
+          </div>
+          <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-800 text-white'>
+            <FontAwesomeIcon icon={faMoneyBill1} />
+            <span className='text-[15px] ml-4 text-gray-200'>Factuación</span>
+          </div>
+          <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-800 text-white'>
+            <FontAwesomeIcon icon={faClock} />
+            <span className='text-[15px] ml-4 text-gray-200'>Reportes</span>
+          </div>
         </div>
-
-        <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-700 text-white'>
-          <span className='text-[15px] ml-4 text-gray-200'>Dashboard</span>
-        </div>
-
-        <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-700 text-white'>
-          <span className='text-[15px] ml-4 text-gray-200'>Apps</span>
-        </div>
-
-        <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-700 text-white'>
-          <span className='text-[15px] ml-4 text-gray-200'>Expertos</span>
-        </div>
-
-        <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-700 text-white'>
-          <span className='text-[15px] ml-4 text-gray-200'>Clientes</span>
-        </div>
-
-        <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-700 text-white'>
-          <span className='text-[15px] ml-4 text-gray-200'>Proyectos</span>
-        </div>
+      )}
     </div>
   );
-};
+}
 
 export default Sidebar;
